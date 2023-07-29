2132e0f33d3755c4c67b0b449400d0a7a6ddb8b5
mohamedreda1258 <mohamed reda.com>
2023-07-29 23:10:27 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,98 @@
+
+	$(".hhhh").counterUp({
+        delay:5,
+        time:1500
+    });
+	$(".hhh").counterUp({
+        delay:5,
+        time:1500
+    });
+
+
+	$(".hh").counterUp({
+        delay:5,
+        time:1500
+    });
+
+
+	$(".h").counterUp({
+        delay:5,
+        time:1500
+    });
+
+/*---------------------------------------------------
+-----------------------------------------------------
+-----------------------------------------------------
+-----------------------------------------------------
+------------------------------------------------------
+-----------------------------------------------------
+*/
+
+function resizeImage1(img){
+    var img =document.getElementById("test1");
+    img.style.width="200px";
+    return img;
+}
+
+
+
+
+
+
+
+
+function  resizeImage2 (img){
+    var img =document.getElementById("test2");
+    img.style.width="200px";
+    return img;
+}
+ 
+
+
+
+function  resizeImage3 (img){
+    var img =document.getElementById("test3");
+    img.style.width="200px";
+    return img;
+}
+
+
+
+
+function  resizeImage4 (img){
+    var img =document.getElementById("test4");
+    img.style.width="200px";
+    return img;
+}
+
+
+
+
+/*--------------------------------*/
+
+
+function  resizeImage5 (img){
+    var img =document.getElementById("test5");
+    img.style.width="200px";
+    return img;
+}
+function  resizeImage6 (img){
+    var img =document.getElementById("test6");
+    img.style.width="200px";
+    return img;
+}
+
+
+
+function  resizeImage7 (img){
+    var img =document.getElementById("test7");
+    img.style.width="200px";
+    return img;
+}
+
+
+function  resizeImage8 (img){
+    var img =document.getElementById("test8");
+    img.style.width="200px";
+    return img;
+}
